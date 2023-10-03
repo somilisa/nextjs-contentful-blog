fbdfea5a385914588cab7a4335428d6388450a62
Lisa <somilisa1010@gmail.com>
2023-10-03 23:36:22 +0100

    feat: nav section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,52 @@
-import Link from 'next/link'
-import { draftMode } from 'next/headers'
+import Link from 'next/link';
+import Head from 'next/head';
+import { draftMode } from 'next/headers';
 
-import Date from './date'
-import CoverImage from './cover-image'
-import Avatar from './avatar'
-import MoreStories from './more-stories'
+import Date from './date';
+import CoverImage from './cover-image';
+import Avatar from './avatar';
+import MoreStories from './more-stories';
 
-import { getAllPosts } from '@/lib/api'
-import { CMS_NAME, CMS_URL } from '@/lib/constants'
+import { getAllPosts } from '@/lib/api';
+import { CMS_NAME, CMS_URL } from '@/lib/constants';
 
 function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
+      <nav className='w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm'>
+        <Link href='/' className='mr-2'>
+          Home
+        </Link>
+        <Link href='/about' className='mr-2'>
+          About
+        </Link>
+        <Link href='/contact' className='mr-2'>
+          Contact
+        </Link>
+        <button>Menu </button>
+      </nav>
+      <h1 className='text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8'>
         Blog.
       </h1>
-      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h2 className='text-center md:text-left text-lg mt-5 md:pl-8'>
         A statically generated blog example using{' '}
         <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
+          href='https://nextjs.org/'
+          className='underline hover:text-success duration-200 transition-colors'
         >
           Next.js
         </a>{' '}
         and{' '}
         <a
           href={CMS_URL}
-          className="underline hover:text-success duration-200 transition-colors"
+          className='underline hover:text-success duration-200 transition-colors'
         >
           {CMS_NAME}
         </a>
         .
       </h2>
     </section>
-  )
+  );
 }
 
 function HeroPost({
@@ -44,46 +57,46 @@ function HeroPost({
   author,
   slug,
 }: {
-  title: string
-  coverImage: any
-  date: string
-  excerpt: string
-  author: any
-  slug: string
+  title: string;
+  coverImage: any;
+  date: string;
+  excerpt: string;
+  author: any;
+  slug: string;
 }) {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
+      <div className='mb-8 md:mb-16'>
         <CoverImage title={title} slug={slug} url={coverImage.url} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className='md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28'>
         <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/posts/${slug}`} className="hover:underline">
+          <h3 className='mb-4 text-4xl lg:text-6xl leading-tight'>
+            <Link href={`/posts/${slug}`} className='hover:underline'>
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className='mb-4 md:mb-0 text-lg'>
             <Date dateString={date} />
           </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+          <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
           {author && <Avatar name={author.name} picture={author.picture} />}
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default async function Page() {
-  const { isEnabled } = draftMode()
-  const allPosts = await getAllPosts(isEnabled)
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const { isEnabled } = draftMode();
+  const allPosts = await getAllPosts(isEnabled);
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
 
   return (
-    <div className="container mx-auto px-5">
+    <div className='container mx-auto px-5'>
       <Intro />
       {heroPost && (
         <HeroPost
@@ -97,5 +110,5 @@ export default async function Page() {
       )}
       <MoreStories morePosts={morePosts} />
     </div>
-  )
+  );
 }
