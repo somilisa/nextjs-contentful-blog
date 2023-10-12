b75a61bb7b894d6017000dedf6209c889147a87d
Lisa <somilisa1010@gmail.com>
2023-10-12 22:59:47 +0100

    style post section #1

--- app/page.tsx
+++ app/page.tsx
@@ -67,10 +67,11 @@ function HeroPost({
 }) {
   return (
     <section>
-      <div className='mb-8 md:mb-16'>
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
-      </div>
+      <div className='mb-8 md:mb-16'></div>
       <div className='md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28'>
+        <div>
+          <CoverImage title={title} slug={slug} url={coverImage.url} />
+        </div>
         <div>
           <h3 className='mb-4 text-4xl lg:text-6xl leading-tight'>
             <Link href={`/posts/${slug}`} className='hover:underline'>
@@ -80,8 +81,6 @@ function HeroPost({
           <div className='mb-4 md:mb-0 text-lg'>
             <Date dateString={date} />
           </div>
-        </div>
-        <div>
           <p className='text-lg leading-relaxed mb-4'>{excerpt}</p>
           {author && <Avatar name={author.name} picture={author.picture} />}
         </div>
